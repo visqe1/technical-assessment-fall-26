1c99e09e1d161f03101d6856ab08872672fa507c
visqe1 <emilyn1806@gmail.com>
2026-04-17 00:54:08 -0400

    add navbar component and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
+import './Navbar.css'
+
 function Navbar() {
-    return <div>Navbar</div>
+    return (
+        <nav>
+            <div className="nav-logo">
+                <div className="logo-sq">SF</div>
+                <span className="nav-title"><span className="red">Scuderia</span> Ferrari</span>
+            </div>
+            <div className="nav-links">
+                <a href="#overview">Overview</a>
+                <a href="#results">Race Results</a>
+                <a href="#charts">Analytics</a>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
